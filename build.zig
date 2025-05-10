b24df389564de39185050256074d986985aea27a
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-05-10 21:34:26 +0200

    add: zprof initial commit

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+/// Build script for the zprof library.
+/// This generates a static library that you can link into your projects.
+pub fn build(b: *std.Build) void {
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    // Create the static library
+    const lib = b.addStaticLibrary(.{
+        .name = "zprof",
+        .root_source_file = .{ .path = "src/zprof.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the library to be installed into the standard location
+    b.installArtifact(lib);
+}
